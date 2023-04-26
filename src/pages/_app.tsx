c8b657497e43410d481d008cd0ea2c4e94b87add
Nick <darkforceyoda@gmail.com>
2023-04-25 22:10:58 -0500

    navbar Layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import '../styles/globals.css';
 import { theme } from '../chakra/theme';
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import Layout from '../components/Layout/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   )
 }
