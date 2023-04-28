3271eadec60c2e959b22cd605a4b74747db9248a
Nick <darkforceyoda@gmail.com>
2023-04-28 18:23:24 -0500

    oauth firebase logic

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,15 +1,23 @@
 import { Button, Image } from '@chakra-ui/react';
-import React from 'react';
+import React, { use } from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { auth } from '../../../firebase/clientApp';
 
 const OAuthButtons: React.FC = () => {
-
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     return (
         <div className='flex flex-col w-full mb-4'>
-            <Button variant='oauth' className='mb-1'>
+            <Button
+                variant='oauth'
+                className='mb-1'
+                isLoading={loading}
+                onClick={() => signInWithGoogle()}
+            >
                 <Image src='/images/googlelogo.png' className='h-[20px]' />
                 Continue with Google
             </Button>
             <Button variant='oauth'>Other</Button>
+            {error && <p className='text-red-600'>{error.message}</p>}
         </div>
     )
 }
