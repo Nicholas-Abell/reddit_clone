c8d08c11dcb51b5b2c44a46114992d50934860a8
Nick <darkforceyoda@gmail.com>
2023-05-02 10:11:47 -0500

    styling width change

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
 type PageContentProps = {
-
+    children: any
 };
 
 const PageContent: React.FC<PageContentProps> = ({ children }) => {
 
     return (
         <div className='flex justify-center px-4'>
-            <div className='flex w-[95%] max-w-[360px]'>
+            <div className='flex w-[95%] max-w-[860px]'>
                 {/* LeftHand Side */}
                 <div className='flex flex-col w-full md:w-[65%] border-2 border-blue-300'>
                     {children && children[0 as keyof typeof children]}
                 </div>
                 {/* RightHand Side */}
-                <div className='hidden md:flex flex-col flex-grow md border-2 border-orange-300'>
+                <div className='hidden sm:flex flex-col flex-grow md border-2 border-orange-300'>
                     {children && children[1 as keyof typeof children]}
                 </div>
             </div>
