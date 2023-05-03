dce1caf8eb5d821ac15b4b29b871ed89b140ce5d
Nick <darkforceyoda@gmail.com>
2023-05-02 19:40:33 -0500

    code cleanup, imported <CreatePostLink />

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -7,6 +7,7 @@ import safeJsonStringify from 'safe-json-stringify';
 import NotFound from '@/src/components/Community/NotFound';
 import Header from '@/src/components/Community/Header';
 import PageContent from '@/src/components/Layout/PageContent';
+import CreatePostLink from '@/src/components/Community/CreatePostLink';
 
 type CommunityPageProps = {
     communityData: Community;
@@ -26,7 +27,7 @@ const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
             <Header communityData={communityData} />
             <PageContent>
                 <>
-                    <div>Left</div>
+                    <CreatePostLink />
                 </>
                 <>
                     <div>Right</div>
