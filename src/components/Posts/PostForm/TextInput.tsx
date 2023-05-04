e3b570edd65228888e00b3e07c5c2d73d9cfdbfa
Nick <darkforceyoda@gmail.com>
2023-05-04 11:03:10 -0500

    conditional rendering for tabs, text input functionality

--- src/components/Posts/PostForm/TextInput.tsx
+++ src/components/Posts/PostForm/TextInput.tsx
@@ -2,7 +2,7 @@ import { Button } from '@chakra-ui/react';
 import React from 'react';
 
 type TextInputProps = {
-    textInputs: {
+    textInput: {
         title: string;
         body: string;
     }
@@ -13,26 +13,33 @@ type TextInputProps = {
     loading: boolean;
 };
 
-const TextInput: React.FC<TextInputProps> = ({ textInputs, onChange, handleCreatePost, loading }) => {
+const TextInput: React.FC<TextInputProps> = ({ textInput, onChange, handleCreatePost, loading }) => {
 
     return (
         <div className='flex flex-col w-full justify-between p-4 gap-4'>
             <input
                 name='title'
-                value={textInputs.title}
+                value={textInput.title}
                 onChange={onChange}
                 placeholder='Title'
                 className='border border-black p-4 bg-gray-100 outline-none rounded placeholder:text-gray-500' />
             <textarea
                 name='body'
-                value={textInputs.body}
+                value={textInput.body}
                 onChange={onChange}
                 className='border border-black p-4 bg-gray-100 outline-none rounded placeholder:text-gray-500'
                 placeholder='Text (optional)'
             />
 
             <div className='flex'>
-                <Button>Post</Button>
+                <Button
+                    padding='0px 30px'
+                    disabled={!textInput.title}
+                    isLoading={loading}
+                    onClick={handleCreatePost}
+                >
+                    Post
+                </Button>
             </div>
         </div>
     )
