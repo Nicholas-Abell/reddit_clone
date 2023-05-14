ddbf95ee7166b05020752700cd645ae1c3f64be0
Nick <darkforceyoda@gmail.com>
2023-05-13 23:00:55 -0500

    usePost:onVote():update firebase

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -6,6 +6,8 @@ import { auth, firestore, storage } from "../firebase/clientApp";
 import { collection, deleteDoc, doc, writeBatch } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
 
+//TypeError: updatedPostVotes is not iterable
+
 const usePosts = () => {
     const [user] = useAuthState(auth);
     const [postStateValue, setPostStateValue] = useRecoilState(postState);
@@ -16,10 +18,15 @@ const usePosts = () => {
             const existingVote = postStateValue.postVotes.find(vote => vote.postId === post.id);
 
             const batch = writeBatch(firestore);
-            const updatedPost = { ...post };
-            const updatedPosts = { ...postStateValue.posts };
-            let updatedPostVotes = { ...postStateValue.postVotes };
+            let updatedPost = { ...post };
+            let updatedPosts = [...postStateValue.posts];
+            let updatedPostVotes = [...postStateValue.postVotes];
             let voteChange = vote;
+
+            // if (!user?.uid) {
+            //     setAuthModalState({ open: true, view: "login" });
+            //     return;
+            // }
 
             //new vote
             if (!existingVote) {
@@ -28,20 +35,25 @@ const usePosts = () => {
                     id: postVoteRef.id,
                     postId: post.id!,
                     communityId,
-                    voteValue: vote,
+                    voteValue: vote, // 1 or -1
                 };
                 batch.set(postVoteRef, newVote);
 
                 updatedPost.voteStatus = voteStatus + vote;
                 updatedPostVotes = [...updatedPostVotes, newVote]
-            } else {
+            }
+            //Remove existing vote 
+            else {
                 const postVoteRef = doc(firestore, 'users', `${user?.uid}/postVotes/${existingVote.id}`);
+                //Remove Vote
                 if (existingVote.voteValue === vote) {
                     updatedPost.voteStatus === voteStatus - vote;
                     updatedPostVotes = updatedPostVotes.filter(vote => vote.id !== existingVote.id);
                     batch.delete(postVoteRef);
                     voteChange *= -1;
-                } else {
+                }
+                //Change Vote 
+                else {
                     updatedPost.voteStatus = voteStatus + 2 * vote;
                     const voteIndex = postStateValue.postVotes.findIndex(vote => vote.id === existingVote.id);
                     updatedPostVotes[voteIndex] = {
@@ -51,17 +63,26 @@ const usePosts = () => {
                     batch.update(postVoteRef, {
                         posts: updatedPostVotes,
                         postVotes: updatedPostVotes
-                    })
+                    });
+
+                    voteChange = 2 * vote;
                 }
             }
 
-            const postIndex = postStateValue.posts.findIndex((item) => item.id === post.id);
-            updatedPosts[postIndex] = updatedPost;
+            //update State
+            const postIndex = postStateValue.posts.findIndex(item => item.id === post.id);
+            updatedPosts[postIndex!] = updatedPost;
             setPostStateValue(prev => ({
                 ...prev,
                 posts: updatedPosts,
                 postVotes: updatedPostVotes
             }));
+
+            //update Firebase
+            const postRef = doc(firestore, 'posts', post.id!);
+            batch.update(postRef, { voteStatus: voteStatus + voteChange })
+
+            await batch.commit();
 
         } catch (error) {
             console.log('OnVote Error: ', error);
@@ -90,6 +111,10 @@ const usePosts = () => {
         }
         return true
     };
+
+    const getCommunityPostVotes = async (communityId: string) => {
+
+    }
 
     return {
         postStateValue,
