52f1082b4b0fa2d15dae7d80793ff1325d595f00
Nick <darkforceyoda@gmail.com>
2023-05-19 22:40:07 -0500

    feat: add comment to post in firebase collection

--- src/components/Posts/Comments/CommentItem.tsx
+++ src/components/Posts/Comments/CommentItem.tsx
@@ -1,28 +1,66 @@
-import { Timestamp } from 'firebase/firestore';
-import React from 'react';
+import { Spinner } from "@chakra-ui/react";
+import { Timestamp } from "firebase/firestore";
+import moment from "moment";
+import React from "react";
+import { FaReddit } from "react-icons/fa";
+import {
+  IoArrowUpCircleOutline,
+  IoArrowDownCircleOutline,
+} from "react-icons/io5";
 
 export type Comment = {
-    id: string;
-    creatorId: string;
-    creatorDisplayText: string;
-    communityId: string;
-    postId: string;
-    postTitle: string;
-    text: string;
-    createdAt: Timestamp;
-}
+  id: string;
+  creatorId: string;
+  creatorDisplayText: string;
+  communityId: string;
+  postId: string;
+  postTitle: string;
+  text: string;
+  createdAt: Timestamp;
+};
 
 type CommentItemProps = {
-  comment: Comment,
+  comment: Comment;
   onDeleteComment: (comment: Comment) => void;
   loadingDelete: boolean;
-  userId: string;  
-}
+  userId: string;
+};
 
-const CommentItem:React.FC<Comment> = ({comment, onDeleteComment, loadingDelete, userId}) => {
-    
-    return (
-        <></>
-    )
-}
+const CommentItem: React.FC<CommentItemProps> = ({
+  comment,
+  onDeleteComment,
+  loadingDelete,
+  userId,
+}) => {
+  return (
+    <div className="flex flex-col pl-10">
+      <div>
+        <FaReddit className="text-gray-300" size={25} />
+      </div>
+      <div className="flex items-center gap-2">
+        <p className="font-bold">{comment.creatorDisplayText}</p>
+        <p className="text-gray-300">
+          {moment(new Date(comment.createdAt.seconds * 1000)).fromNow()}
+        </p>
+      </div>
+      {loadingDelete && <Spinner size="sm" />}
+      <p>{comment.text}</p>
+      <div className="flex items-center cursor-pointer text-gray-500 p-2 gap-4">
+        <IoArrowUpCircleOutline size={25} />
+        <IoArrowDownCircleOutline size={25} />
+        {userId === comment.creatorId && (
+          <>
+            <p className="text-blue-400">Edit</p>
+            <p
+              className="text-blue-400"
+              onClick={() => onDeleteComment(comment)}
+            >
+              Delete
+            </p>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 export default CommentItem;
--- src/components/Posts/Comments/Comments.tsx
+++ src/components/Posts/Comments/Comments.tsx
@@ -1,100 +1,112 @@
-import { Post, postState } from '@/src/atoms/PostAtom';
-import { User } from 'firebase/auth';
-import React, { useEffect, useState } from 'react';
-import CommentInput from './CommentInput';
-import { Timestamp, collection, doc, increment, serverTimestamp, writeBatch } from 'firebase/firestore';
-import { firestore } from '@/src/firebase/clientApp';
-import { useSetRecoilState } from 'recoil';
-import {Comment} from './CommentItem';
+import { Post, postState } from "@/src/atoms/PostAtom";
+import { User } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import CommentInput from "./CommentInput";
+import {
+  Timestamp,
+  collection,
+  doc,
+  increment,
+  serverTimestamp,
+  writeBatch,
+} from "firebase/firestore";
+import { firestore } from "@/src/firebase/clientApp";
+import { useSetRecoilState } from "recoil";
+import CommentItem, { Comment } from "./CommentItem";
 
 type CommentsProps = {
-    user: User;
-    selectedPost: Post | null;
-    communityId: string;
+  user: User;
+  selectedPost: Post | null;
+  communityId: string;
 };
 
-const Comments: React.FC<CommentsProps> = ({ user, selectedPost, communityId }) => {
-    const [commentText, setCommentText] = useState('');
-    const [comments, setComments] = useState<Comment[]>([]);
-    const [fetchLoading, setFetchLoading] = useState(false);
-    const [createLoading, setCreateLoading] = useState(false);
-    const setPostState = useSetRecoilState(postState);
+const Comments: React.FC<CommentsProps> = ({
+  user,
+  selectedPost,
+  communityId,
+}) => {
+  const [commentText, setCommentText] = useState("");
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [fetchLoading, setFetchLoading] = useState(false);
+  const [createLoading, setCreateLoading] = useState(false);
+  const setPostState = useSetRecoilState(postState);
 
-    const onCreateComment = async (commentText: string) => {
-        setCreateLoading(true);
-            try {
-                const batch = writeBatch(firestore);
-                //create document
-                const commentDocRef = doc(collection(firestore, 'comments'));  
-         
-                const newComment: Comment = {
-                    id: commentDocRef.id,
-                    creatorId: user.uid,
-                    creatorDisplayText: user.email!.split('@')[0],
-                    communityId,
-                    postId: selectedPost?.id!,
-                    postTitle: selectedPost?.title!,
-                    text: commentText,
-                    createdAt: serverTimestamp() as Timestamp,
-                }
-                
-                batch.set(commentDocRef, newComment);
+  const onCreateComment = async (commentText: string) => {
+    setCreateLoading(true);
+    try {
+      const batch = writeBatch(firestore);
+      //create document
+      const commentDocRef = doc(collection(firestore, "comments"));
 
-                            //update post numberOfComments
-                const postDocRef = doc(firestore, 'posts', selectedPost?.id!);
-                batch.update(postDocRef, {
-                    numberOfComments: increment(1),
-                });
+      const newComment: Comment = {
+        id: commentDocRef.id,
+        creatorId: user.uid,
+        creatorDisplayText: user.email!.split("@")[0],
+        communityId,
+        postId: selectedPost?.id!,
+        postTitle: selectedPost?.title!,
+        text: commentText,
+        createdAt: serverTimestamp() as Timestamp,
+      };
 
-                await batch.commit();
+      batch.set(commentDocRef, newComment);
 
-            // update client recoil state
-                setCommentText('');
-                setComments((prev) => ([
-                    newComment,
-                    ...prev,
-                ]));
-                setPostState((prev) => ({
-                    ...prev,
-                    numberOfComments: prev.selectedPost?.numberOfComments! + 1,
-                }))
+      newComment.createdAt = { seconds: Date.now() / 1000 } as Timestamp;
 
-                setCreateLoading(false);
+      //update post numberOfComments
+      const postDocRef = doc(firestore, "posts", selectedPost?.id!);
+      batch.update(postDocRef, {
+        numberOfComments: increment(1),
+      });
 
-            } catch (error) {
-                
-            }
-    }
+      await batch.commit();
 
-    const onDeleteComment = async (comment: any) => {
-            //delete document
+      // update client recoil state
+      setCommentText("");
+      setComments((prev) => [newComment, ...prev]);
+      setPostState((prev) => ({
+        ...prev,
+        numberOfComments: prev.selectedPost?.numberOfComments! + 1,
+      }));
 
-            //update post numberOfComments
+      setCreateLoading(false);
+    } catch (error) {}
+  };
 
-            // update client recoil state
-    }
+  const onDeleteComment = async (comment: any) => {
+    //delete document
+    //update post numberOfComments
+    // update client recoil state
+  };
 
-    const getPostComments = async () => {
+  const getPostComments = async () => {};
 
-    }
+  useEffect(() => {
+    getPostComments();
+  }, []);
 
-    useEffect(() => {
-        getPostComments();
-    }, []);
-
-    return (
-        <div className='bg-white'>
-            <div className='flex flex-col pr-4 pl-10 mb-6 w-full'>            
-            <CommentInput
-             commentText={commentText}
-             setCommentText={setCommentText}
-             user={user}
-             createLoading={createLoading}
-             onCreateComment={onCreateComment}
-            />
-            </div>
-        </div>
-
-    )
-}
+  return (
+    <div className="bg-white">
+      <div className="flex flex-col pr-4 pl-10 mb-6 w-full">
+        <CommentInput
+          commentText={commentText}
+          setCommentText={setCommentText}
+          user={user}
+          createLoading={createLoading}
+          onCreateComment={onCreateComment}
+        />
+      </div>
+      <div className="flex flex-col gap-6">
+        {comments.map((comment) => (
+          <CommentItem
+            comment={comment}
+            onDeleteComment={onDeleteComment}
+            loadingDelete={false}
+            userId={user?.uid}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 export default Comments;
