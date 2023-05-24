17d0c0da9c7218c51a95c8d7e44cad879cd1e26d
Nick <darkforceyoda@gmail.com>
2023-05-24 10:46:38 -0500

    PageContent padding change

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
+import React from "react";
 
 type PageContentProps = {
-    children: any
+  children: any;
 };
 
 const PageContent: React.FC<PageContentProps> = ({ children }) => {
-
-    return (
-        <div className='flex justify-centerg px-4'>
-            <div className='flex w-[95%] max-w-[860px]'>
-                {/* LeftHand Side */}
-                <div className='flex flex-col w-full md:w-[65%] md:mr-6'>
-                    {children && children[0 as keyof typeof children]}
-                </div>
-                {/* RightHand Side */}
-                <div className='hidden md:flex flex-col flex-grow'>
-                    {children && children[1 as keyof typeof children]}
-                </div>
-            </div>
+  return (
+    <div className="flex justify-centerg px-4 pt-4">
+      <div className="flex w-[95%] max-w-[860px]">
+        {/* LeftHand Side */}
+        <div className="flex flex-col w-full md:w-[65%] md:mr-6">
+          {children && children[0 as keyof typeof children]}
         </div>
-    )
-}
+        {/* RightHand Side */}
+        <div className="hidden md:flex flex-col flex-grow">
+          {children && children[1 as keyof typeof children]}
+        </div>
+      </div>
+    </div>
+  );
+};
 export default PageContent;
